5788c9ffdf9a8bb4f81219c6cb81ef6f5c9f797c
Villa1220 <andresvillacis1220@gmail.com>
2024-10-31 18:19:39 -0500

    "se hizo un comentario"

--- sistema-reservas-backend/index.js
+++ sistema-reservas-backend/index.js
@@ -14,6 +14,6 @@ const menuRoutes = require('./routes/menuRoutes');
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes); 
 app.use('/api/menu', menuRoutes);
-
+//comentario
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor en el puerto ${PORT}`));
